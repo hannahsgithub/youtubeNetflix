9548a9ea51cc690559755e2029a6f83dc093407c
hannahsgithub <ha.yi.song9@gmail.com>
2023-06-04 17:36:57 -0400

    added youtube video playing in background

--- src/components/P1HomePage/P1HomePage.tsx
+++ src/components/P1HomePage/P1HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { memo } from 'react';
 import type { FC } from 'react';
 
@@ -10,14 +11,32 @@ import { YoutubeLogo } from './YoutubeLogo/YoutubeLogo';
 interface Props {
   className?: string;
 }
-/* @figmaId 13:412 */
+
 export const P1HomePage: FC<Props> = memo(function P1HomePage(props = {}) {
+  const [shrinkTitle, setShrinkTitle] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShrinkTitle(true);
+    }, 4000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className={`${resets.programResets} ${classes.root}`}>
+      <div className={classes.videoContainer}>
+        <iframe
+          className={classes.youtubeVideo}
+          src="https://www.youtube.com/embed/shW9i6k8cB0?autoplay=1&loop=1&playlist=shW9i6k8cB0&mute=0"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
       <div className={classes.searchBar}>
         <div className={classes.searchBarContent}>
-          <div className={classes.search}>Search</div>
-          <div className={classes.oval}></div>
+          <input type="text" className={classes.search} placeholder="Search" />
         </div>
       </div>
       <YoutubeLogo
@@ -31,11 +50,17 @@ export const P1HomePage: FC<Props> = memo(function P1HomePage(props = {}) {
           ),
         }}
       />
-      <PlayButton className={classes.playButton} />
-      <div className={classes._subtitle}>#1 Trending in Canada </div>
-      <div className={classes.title}>
-        <div className={classes.textBlock}>SPIDER-MAN: ACROSS THE SPIDER-VERSE</div>
-        <div className={classes.textBlock2}>Official Trailer #2 </div>
+      <a href="https://www.youtube.com/watch?v=shW9i6k8cB0&t=3s&ab_channel=SonyPicturesEntertainment" target="_blank" rel="noopener noreferrer">
+        <PlayButton className={classes.playButton} />
+      </a>
+      <div className={`${classes._subtitle} ${shrinkTitle ? classes.shrink : ''}`}>
+        #1 Trending in Canada
+      </div>
+      <div className={`${classes.title} ${shrinkTitle ? classes.shrink : ''}`}>
+        <div className={`${classes.textBlock} ${shrinkTitle ? classes.shrink : ''}`} style={{ fontSize: shrinkTitle ? '14px' : '48px' }}>
+          SPIDER-MAN: ACROSS THE SPIDER-VERSE
+        </div>
+        <div className={`${classes.textBlock2} ${shrinkTitle ? classes.shrink : ''}`}>Official Trailer #2</div>
       </div>
     </div>
   );
